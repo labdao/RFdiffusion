a533c74191e2230f1e0440942da6a8db8bc22123
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2023-04-17 11:49:00 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 cd /app && source activate SE3nv
-./scripts/run_inference.py 'contigmap.contigs=[A59-80/0 10-100]' inference.input_pdb=/app/examples/input_pdbs/insulin_target.pdb inference.output_prefix=/outputs/insulin_design.pdb inference.num_designs=10 denoiser.noise_scale_ca=0 denoiser.noise_scale_frame=0
+./scripts/run_inference.py 'contigmap.contigs=[A59-80/0 10-100]' inference.input_pdb=/app/examples/input_pdbs/insulin_target.pdb inference.output_prefix=/outputs/insulin_backbone inference.num_designs=10 denoiser.noise_scale_ca=0 denoiser.noise_scale_frame=0
